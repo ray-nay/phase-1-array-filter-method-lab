2946e31b374b993d21cb48baac84ae58d93f7fe2
Renee Mumbi <1723renee@gmail.com>
2022-12-08 18:38:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    //let driver;
+    return drivers.filter(function(item){
+        if (item === name) {
+            return item
+        } else{
+            if (name.toLowerCase() === item.toLowerCase()) {
+                return item
+            }
+        }
+    })
+    //return driver
+}
+
+
+
+function fuzzyMatch(drivers, query) {
+   return drivers.filter(function(item) {
+        return item.startsWith(query)
+      //return item.slice(0, 1).indexOf(query.toLowerCase()) !== -1
+    })
+    //console.log(result)
+  }
+
+function matchName (drivers, name){
+    return drivers.filter(function (driver) {
+        return driver.name === name
+    })
+}
